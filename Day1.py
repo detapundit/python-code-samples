7d1931cebc88902a868cbec1198b3b2de7b722a0
detapundit <detapundit@gmail.com>
2026-06-05 19:50:59 +0530

    Day1

--- Day1.py
+++ Day1.py
@@ -2,3 +2,17 @@ print("Hello World")
 print("Hello world!\nHello world!\nHello world!\n")
 print("Hello" + " Angela")
 
+
+print("Hello " + input("Enter your name: ") + "!")
+
+name = input("What is your name?")
+print(name)
+l1 = len(name)
+print(l1)
+
+
+print("Welcome to the Band Name Generator.")
+city = input("What city did you grow up?\n")
+pet_name = input("What is your pet name?\n")
+
+print("Name of your band is: " + city + " " + pet_name)
